51f3c93712b6883cda2eea6f4e2a1b9ac8ab328b
h4yv3n <59025997+h4yv3n@users.noreply.github.com>
2022-04-08 15:48:06 +0200

    Create euler3.cc

---
+++ project euler no3/euler3.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    unsigned long long int number = 600851475143;
+
+    bool isPrime = 1;
+
+    int lastPrime = 2;
+
+    for (int i = lastPrime; i < number; i++)
+    {
+        std::cout << number << std::endl;
+        std::cout << i << std::endl;
+        isPrime = 1;
+
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = 0;
+            }
+        }
+
+        if (isPrime && (number % i == 0))
+        {
+            lastPrime = i;
+            number = number / i;
+        }
+    }
+
+    std::cout << number;
+    std::cin.get();
+
+    return 0;
+}
